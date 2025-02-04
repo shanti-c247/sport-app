54332e4f4178392efd8fba29cc92cf9a3f11df8b
alok <alok.p@chapter247.com>
2025-02-04 17:04:00 +0530

    upload file

--- src/models/tamplateB.model.ts
+++ src/models/tamplateB.model.ts
@@ -1,10 +1,11 @@
+import { commonVariables } from '@constants';
 import { IBio, IEvent, IImage, ITemplateB } from '@customTypes';
 import { Schema, model, Document } from 'mongoose';
 
 
 const ImageSchema = new Schema<IImage>({
-  id: { type: Number, default: null },
-  src: { type: String,default: null },
+  // id: { type: Number, default: null },
+  src: { type: String, default: null },
   description: { type: String, default: null },
   isBanner: { type: Boolean, default: null }
 });
@@ -12,19 +13,33 @@ const ImageSchema = new Schema<IImage>({
 const BioSchema = new Schema<IBio>({
   profileImage: { type: String, default: null },
   title: { type: String, default: null },
-  content: { type: String,default: null}
+  content: { type: String, default: null }
 });
 
 const EventSchema = new Schema<IEvent>({
   title: { type: String, default: null },
-  eventDate: { type: String,default: null},
-  description: { type: String,default: null }
+  eventDate: { type: String, default: null },
+  description: { type: String, default: null }
 });
 
 const TemplateBSchema = new Schema<ITemplateB>({
   slider: { type: [ImageSchema], default: null },
   bio: { type: BioSchema, default: null },
-  event: { type: [EventSchema],default: null }
+  event: { type: [EventSchema], default: null },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+},
+  {
+    timestamps: true,
+  },);
+// Customize JSON transformation to exclude sensitive data
+TemplateBSchema.set('toJSON', {
+  transform: (_doc, ret) => {
+    ret.id = ret._id; // Add id
+    commonVariables.EVENT_SENSITIVE_INFO_DB_COLUMNS.forEach((field) => delete ret[field]);
+    return ret;
+  },
 });
-
 export const TemplateB = model<ITemplateB>('templateB', TemplateBSchema);
